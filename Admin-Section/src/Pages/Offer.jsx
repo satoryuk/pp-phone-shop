040f1bd4cf9efafa6780be5a61fe4e057f31563b
ChanSenghak <163015944+ChanSenghak@users.noreply.github.com>
2024-11-24 19:52:08 +0700

    Update Offer.jsx

--- Admin-Section/src/Pages/Offer.jsx
+++ Admin-Section/src/Pages/Offer.jsx
@@ -4,41 +4,41 @@ const Offer = () => {
   return (
     <Details />
   );
-}
+};
 
 function Details() {
   return (
-    <div className="container mx-auto bg-gray-100 rounded-lg shadow-[0_100px_50px_rgba(0,0,0,0.3)] max-w-md mt-12 p-6">
-      <h1 className="text-2xl font-bold text-green-600 mb-4">Inventory Detail</h1>
+    <div className="container mx-auto bg-white rounded-lg shadow-[0_40px_100px_rgba(0,0,0,0.4)] max-w-lg mt-6 p-10 transform rotate-0">
+      <h1 className="text-3xl font-bold text-green-600 mb-6 text-center">Inventory Detail</h1>
       <div className="flex flex-col items-center md:flex-row md:items-start">
         <img 
-          src="https://pin.it/516G85ZIt" // Replace with your image URL
+          src="https://i.pinimg.com/564x/ca/43/a1/ca43a11d6672b910f1c19b2c537ba2da.jpg"
           alt="Product"
-          className="w-48 h-48 object-cover mb-4 md:mb-0 md:mr-6 rounded-lg"
+          className="w-56 h-56 object-cover mb-6 md:mb-0 md:mr-8 rounded-lg"
         />
         <div className="w-full">
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Product code:</span> 123
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Price:</span> 1000$
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Category:</span> iPhone
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Inventory:</span> 100
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Subtotal:</span> 1000$
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Description:</span> Nigaaaaaaaa
           </div>
-          <div className="mb-2">
+          <div className="mb-3">
             <span className="font-semibold">Total:</span> 900$
           </div>
-          <button className="mt-4 bg-green-500 text-white py-2 px-6 rounded-lg hover:bg-green-600">
+          <button className="mt-6 bg-green-500 text-white py-3 px-8 rounded-lg hover:bg-green-600">
             Update
           </button>
         </div>
@@ -48,3 +48,4 @@ function Details() {
 }
 
 export default Offer;
+
