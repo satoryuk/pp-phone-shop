a67e1adac9ee858d093c2d00711ce548f898b020
keosambath <keosambathnov@gmail.com>
2024-11-25 09:55:39 +0700

    revert code

--- User-Section/src/Pages/auth/Login.jsx
+++ User-Section/src/Pages/auth/Login.jsx
@@ -1,21 +1,50 @@
-import React from "react";
-import {
-    AiOutlineEyeInvisible,
-    AiOutlineLock,
-    AiOutlineLogout,
-    AiOutlineMail,
-    AiOutlineUser,
-} from "react-icons/ai";
-import Navbar from "../home/Navbar";
-import { Spacer, XTextfield } from "./SignUpScreen";
-import { XButton } from "./SignUpScreen";
+
+import React, { useState } from 'react';
+import { AiOutlineEyeInvisible, AiOutlineLock, AiOutlineLogout, AiOutlineMail, AiOutlineUser,AiOutlineEye } from 'react-icons/ai';
+import Navbar from '../home/Navbar';
+import { Spacer } from './SignUpScreen';
+import { XTextfield } from '../../Conponents/Bath_Component';
+import { XButton } from './SignUpScreen';
+
+import { Link } from 'react-router-dom';
+
 const Login = () => {
+    const[passwordToggle, setPasswordToggle] = useState(false)
+    const [passwordState, setPassword] = useState("")
+    const [passwordStrength, setPasswordStrength] = useState("");
+
+    const toggleValidateIcon = ()=>{
+        setPasswordToggle(!passwordToggle)
+    }
+    const handlePasswordLength = (value) => {
+        setPassword(value)
+        validatePasswordLength(value)
+    }
+    const validatePasswordLength = (passwordState) => {
+        if (passwordState.length === 0) {
+            setPasswordStrength("");
+        } else if (passwordState.length < 8) {
+            setPasswordStrength("Weak");
+        } else {
+            setPasswordStrength("Strong");
+        }
+    }
+    const handleColorPasswordStrength = (passStrength) => {
+        switch (passStrength) {
+            case "Weak": return "red";
+            case "Strong": return "green";
+            default: 
+            return "gray";
+        }
+    }
+
     return (
         <div>
             <Navbar />
 
             <div className="flex flex-col items-center min-h-screen bg-gray-100">
                 <div className="flex flex-col items-center w-full max-w-md bg-white p-6 rounded-lg shadow-md mt-6">
+
                     <h1 className="text-green-600 text-3xl font-bold mb-4 ">Sign In</h1>
                     <XTextfield
                         label="Full Name"
@@ -33,22 +62,37 @@ const Login = () => {
                         label="Password"
                         placeholder="X_AE_A13b"
                         icon={<AiOutlineLock />}
-                        suffixIcon={<AiOutlineEyeInvisible />}
+                        validation={validatePasswordLength}
+                        onValueChange={handlePasswordLength}
+                        suffixIcon={passwordToggle ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+                        onClick={toggleValidateIcon}
+                        inputType={
+                            passwordToggle ? "text" : "password"
+                        }
                     />
                     <Spacer width={null} height={5} />
-                    <h1>Password strength: Strong </h1>
+                    {passwordState && (
+                        <p
+                            className={`text-${handleColorPasswordStrength(passwordStrength)
+                                }-500 font-semibold mt-2`}
+                        >
+                            Password strength: {passwordStrength}
+                        </p>
+                    )}
                     <Spacer width={null} />
                     <XButton label="Sign In" icon={<AiOutlineLogout />} />
                     <Spacer width={null} />
-                    <div className="flex items-center justify-center">
-                        <h2 className="font-semibold">Don't have account yet?</h2>
+                    <div className='flex items-center justify-center'>
+                        <h2 className='font-semibold'>Don't have account yet?</h2>
                         <Spacer width={5} />
-                        <h2 className="text-green-600  font-semibold">Sign Up.</h2>
+                        <Link to="/auth/Signup">
+                            <h2 className='text-green-600  font-semibold' >Sign Up.</h2></Link>
                     </div>
                 </div>
             </div>
         </div>
-    );
-};
 
-export default Login;
+    )
+}
+
+export default Login
