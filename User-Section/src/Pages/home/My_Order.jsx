8cf38ce9700fe2a8afe128300617bce6fbd689aa
keosambath <keosambathnov@gmail.com>
2024-12-07 15:08:52 +0700

    order page

---
+++ User-Section/src/Pages/home/My_Order.jsx
@@ -0,0 +1,8 @@
+const MyOrderPage = () => {
+    return (
+        <div>
+            <h1 className="text-green-600 text-3xl font-bold mb-4 ">My Order</h1>
+        </div>
+    )
+}
+export default MyOrderPage;
