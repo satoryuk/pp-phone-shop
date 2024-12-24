2723ced9ade35a4db79d97f11e41e9abda86bcae
kongming <155872351+endsy2@users.noreply.github.com>
2024-12-24 09:48:16 +0700

    new

--- API/index.js
+++ API/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static('uploads'))
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://phone-shop-website.vercel.app"],
     credentials: true
   }
 ));
