f62a80192126633d17b471ed844aa7131529ef04
satoryuk <your_email@example.comthong.vathana.2822@rupp.edu.kh>
2025-04-30 07:34:21 +0700

    new update

--- Admin-Section/src/Utils/Model/UpdateProductVariant.jsx
+++ Admin-Section/src/Utils/Model/UpdateProductVariant.jsx
@@ -2,154 +2,145 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { updateProductVariants } from "../../Fetch/FetchAPI";
 
-
 const UpdateProductVariants = ({ product_id }) => {
-    const location = useLocation();
-    const [id, setID] = useState('');
-    const [images, setImages] = useState([]);
+  const location = useLocation();
+  const [id, setID] = useState("");
+  const [images, setImages] = useState([]);
 
-    const [colors, setColors] = useState("#000000"); // Default to one color
+  const [colors, setColors] = useState("#000000"); // Default to one color
 
+  useEffect(() => {
+    setID(product_id);
+  }, [product_id]);
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files); // Convert FileList to Array
+    setImages([...images, ...files]); // Append new files to the existing ones
+  };
 
-    useEffect(() => {
-        setID(product_id)
-    }, [product_id])
-    const handleImageChange = (e) => {
-        const files = Array.from(e.target.files); // Convert FileList to Array
-        setImages([...images, ...files]); // Append new files to the existing ones
+  const handleRemoveImage = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
+
+  // const handleSubmit = async (e) => {
+  //     e.preventDefault();
+  //     const formdata = {
+  //         images,
+  //         price,
+  //         colors,
+  //         stock
+  //     }
+  //     try {
+  //         const result = await addNewProductAPI(formdata, id);
+  //         // console.log(formdata.colors);
+
+  //         // console.log(formdata.images);
+
+  //         // handleClear(); // Clear form after successful submission
+  //         console.log(result);
+  //     } catch (error) {
+  //         console.log(error);
+  //     }
+  // };
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const formdata = {
+      images,
+
+      colors,
     };
-
-    const handleRemoveImage = (index) => {
-        const newImages = [...images];
-        newImages.splice(index, 1);
-        setImages(newImages);
-    };
-
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const formdata = {
-    //         images,
-    //         price,
-    //         colors,
-    //         stock
-    //     }
-    //     try {
-    //         const result = await addNewProductAPI(formdata, id);
-    //         // console.log(formdata.colors);
-
-    //         // console.log(formdata.images);
-
-    //         // handleClear(); // Clear form after successful submission
-    //         console.log(result);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-    const handleUpdate = async (e) => {
-
-        e.preventDefault();
-        const formdata = {
-            images,
-
-            colors,
-
-        }
-        try {
-            const result = await updateProductVariants(formdata, id);
-            window.location.reload();
-            console.log(result);
-
-        } catch (error) {
-            console.log(error);
-        }
+    try {
+      const result = await updateProductVariants(formdata, id);
+      window.location.reload();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const handleClear = () => {
+    setImages([]);
 
-    const handleClear = () => {
-        setImages([]);
+    setColors([""]);
+  };
+  return (
+    <div className="bg-white border-gray-300 border p-12 rounded-lg w-full max-w-9xl mx-auto mt-12 shadow-lg">
+      <h1 className="text-center text-3xl text-blue-600 font-bold mb-8">
+        <h1>Update Product</h1>
+      </h1>
+      <form
+        onSubmit={handleUpdate}
+        className="grid grid-cols-1   xl:grid-cols-2 md:items-center gap-10 py-10 "
+      >
+        {/* Colors Section */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Colors
+          </label>
+          <div className="flex flex-col gap-4 bg-gray-50 p-4 rounded-lg shadow-md">
+            <div className="flex items-center gap-6">
+              {/* Color Picker */}
+              <div className="flex items-center gap-2">
+                <input
+                  type="color"
+                  value={colors}
+                  onChange={(e) => setColors(e.target.value)}
+                  className="h-6 w-56 rounded-lg border-gray-300 shadow-sm focus:ring-blue-600 focus:border-blue-600"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* image */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Product Images
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            multiple // This allows multiple files selection
+            onChange={handleImageChange}
+            className="h-10 w-full rounded-lg border border-gray-300 p-1 mb-2"
+          />
+          <div className="flex flex-wrap gap-4 mt-2">
+            {images.map((image, index) => (
+              <div key={index} className="relative">
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt={`Product Image ${index + 1}`}
+                  className="h-16 w-16 object-cover rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveImage(index)}
+                  className="absolute top-0 right-0 text-red-600"
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
 
-        setColors(['']);
-
-
-    }
-    return (
-        <div className="bg-white border-gray-300 border p-12 rounded-lg w-full max-w-9xl mx-auto mt-12 shadow-lg">
-            <h1 className="text-center text-3xl text-primary font-bold mb-8">
-                <h1>Update Product</h1>
-            </h1>
-            <form
-                onSubmit={handleUpdate}
-                className="grid grid-cols-1   xl:grid-cols-2 md:items-center gap-10 py-10 "
-            >
-
-
-                {/* Colors Section */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Colors</label>
-                    <div className="flex flex-col gap-4 bg-gray-50 p-4 rounded-lg shadow-md">
-                        <div className="flex items-center gap-6">
-                            {/* Color Picker */}
-                            <div className="flex items-center gap-2">
-                                <input
-                                    type="color"
-                                    value={colors}
-                                    onChange={(e) => setColors(e.target.value)}
-                                    className="h-6 w-56 rounded-lg border-gray-300 shadow-sm focus:ring-primary focus:border-primary"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* image */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">
-                        Product Images
-                    </label>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        multiple // This allows multiple files selection
-                        onChange={handleImageChange}
-                        className="h-10 w-full rounded-lg border border-gray-300 p-1 mb-2"
-                    />
-                    <div className="flex flex-wrap gap-4 mt-2">
-                        {images.map((image, index) => (
-                            <div key={index} className="relative">
-                                <img
-                                    src={URL.createObjectURL(image)}
-                                    alt={`Product Image ${index + 1}`}
-                                    className="h-16 w-16 object-cover rounded-lg"
-                                />
-                                <button
-                                    type="button"
-                                    onClick={() => handleRemoveImage(index)}
-                                    className="absolute top-0 right-0 text-red-600"
-                                >
-                                    X
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-
-                <div className=" w-full gap-4 mt-4 grid grid-cols-2 justify-center items-center">
-                    <input
-                        type="submit"
-                        value="submit"
-                        className="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-2 rounded-lg transition"
-                    />
-                    <button
-                        type="button"
-                        onClick={(e) => handleClear(e)}
-                        className="bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-2 rounded-lg transition"
-                    >
-                        Clear
-                    </button>
-                </div>
-            </form >
-        </div >
-    );
+        <div className=" w-full gap-4 mt-4 grid grid-cols-2 justify-center items-center">
+          <input
+            type="submit"
+            value="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-lg transition"
+          />
+          <button
+            type="button"
+            onClick={(e) => handleClear(e)}
+            className="bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-2 rounded-lg transition"
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UpdateProductVariants;
--- Admin-Section/src/Utils/Model/UpdateSpec.jsx
+++ Admin-Section/src/Utils/Model/UpdateSpec.jsx
@@ -3,190 +3,205 @@ import { useLocation } from "react-router-dom";
 import { updateProductVariants, updateSpec } from "../../Fetch/FetchAPI";
 
 const UpdateSpec = ({ product_id, storage }) => {
-    const location = useLocation();
-    const [id, setID] = useState("");
-    const [oldStorage, setOldStorage] = useState("");
-    const [newStorage, setNewStorage] = useState("");
-    const [processor, setProcessor] = useState("");
-    const [ram, setRam] = useState("");
-    const [battery, setBattery] = useState("");
-    const [camera, setCamera] = useState("");
-    const [screen_size, setScreen_size] = useState("")
-    const [price, setPrice] = useState("");
-    const [stock, setStock] = useState("");
+  const location = useLocation();
+  const [id, setID] = useState("");
+  const [oldStorage, setOldStorage] = useState("");
+  const [newStorage, setNewStorage] = useState("");
+  const [processor, setProcessor] = useState("");
+  const [ram, setRam] = useState("");
+  const [battery, setBattery] = useState("");
+  const [camera, setCamera] = useState("");
+  const [screen_size, setScreen_size] = useState("");
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
 
-    useEffect(() => {
-        setID(product_id);
-        setOldStorage(storage)
-    }, [product_id]);
+  useEffect(() => {
+    setID(product_id);
+    setOldStorage(storage);
+  }, [product_id]);
 
-    const handleUpdate = async (e) => {
-        e.preventDefault();
-        const formdata = {
-            newStorage,
-            processor,
-            ram,
-            battery,
-            camera,
-            price,
-            stock,
-            screen_size,
-        };
-        const queryParam = {
-            variantID: id,
-            oldStorage
-        }
-        try {
-            const result = await updateSpec(formdata, queryParam);
-            window.location.reload();
-            console.log(result);
-            // console.log(formdata);
-            // console.log(queryParam);
-
-        } catch (error) {
-            console.log(error);
-        }
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const formdata = {
+      newStorage,
+      processor,
+      ram,
+      battery,
+      camera,
+      price,
+      stock,
+      screen_size,
     };
-
-    const handleClear = () => {
-        setOldStorage("");
-        setNewStorage("");
-        setProcessor("");
-        setRam("");
-        setBattery("");
-        setCamera("");
-        setPrice("");
-        setStock("");
+    const queryParam = {
+      variantID: id,
+      oldStorage,
     };
+    try {
+      const result = await updateSpec(formdata, queryParam);
+      window.location.reload();
+      console.log(result);
+      // console.log(formdata);
+      // console.log(queryParam);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="bg-white border-gray-300 border p-12 rounded-lg w-full max-w-9xl mx-auto mt-12 shadow-lg">
-            <h1 className="text-center text-3xl text-primary font-bold mb-8">Update Product</h1>
-            <form
-                onSubmit={handleUpdate}
-                className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:items-center gap-10 py-10"
-            >
-                {/* Price */}
+  const handleClear = () => {
+    setOldStorage("");
+    setNewStorage("");
+    setProcessor("");
+    setRam("");
+    setBattery("");
+    setCamera("");
+    setPrice("");
+    setStock("");
+  };
 
-                {/* Storage */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Storage</label>
-                    <input
-                        type="text"
-                        name="storage"
-                        placeholder="Enter new storage capacity"
-                        value={newStorage}
-                        className="input-style"
-                        onChange={(e) => setNewStorage(e.target.value)}
-                    />
-                </div>
+  return (
+    <div className="bg-white border-gray-300 border p-12 rounded-lg w-full max-w-9xl mx-auto mt-12 shadow-lg">
+      <h1 className="text-center text-3xl text-blue-600 font-bold mb-8">
+        Update Product
+      </h1>
+      <form
+        onSubmit={handleUpdate}
+        className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:items-center gap-10 py-10"
+      >
+        {/* Price */}
 
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Price</label>
-                    <input
-                        type="number"
-                        name="price"
-                        placeholder="Enter product price"
-                        value={price}
-                        className="input-style"
-                        onChange={(e) => setPrice(e.target.value)}
-                        min="0"
-                        required
-                    />
-                </div>
-                {/* Processor */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Processor</label>
-                    <input
-                        type="text"
-                        name="processor"
-                        placeholder="Enter processor details"
-                        value={processor}
-                        className="input-style"
-                        onChange={(e) => setProcessor(e.target.value)}
-                    />
-                </div>
-
-                {/* RAM */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">RAM</label>
-                    <input
-                        type="text"
-                        name="ram"
-                        placeholder="Enter RAM size"
-                        value={ram}
-                        className="input-style"
-                        onChange={(e) => setRam(e.target.value)}
-                    />
-                </div>
-
-                {/* Battery */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Battery</label>
-                    <input
-                        type="text"
-                        name="battery"
-                        placeholder="Enter battery capacity"
-                        value={battery}
-                        className="input-style"
-                        onChange={(e) => setBattery(e.target.value)}
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Screen Size</label>
-                    <input
-                        type="text"
-                        name="battery"
-                        placeholder="Enter Screen Size"
-                        value={screen_size}
-                        className="input-style"
-                        onChange={(e) => setScreen_size(e.target.value)}
-                    />
-                </div>
-
-                {/* Camera */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Camera</label>
-                    <input
-                        type="text"
-                        name="camera"
-                        placeholder="Enter camera specifications"
-                        value={camera}
-                        className="input-style"
-                        onChange={(e) => setCamera(e.target.value)}
-                    />
-                </div>
-
-                {/* Stock */}
-                <div className="flex flex-col">
-                    <label className="text-sm font-medium text-primary mb-2">Stock</label>
-                    <input
-                        type="number"
-                        name="stock"
-                        placeholder="Enter stock quantity"
-                        value={stock}
-                        className="input-style"
-                        onChange={(e) => setStock(e.target.value)}
-                    />
-                </div>
-
-                <div className="w-full gap-4 mt-4 grid grid-cols-2 justify-center items-center">
-                    <input
-                        type="submit"
-                        value="Submit"
-                        className="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-2 rounded-lg transition"
-                    />
-                    <button
-                        type="button"
-                        onClick={handleClear}
-                        className="bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-2 rounded-lg transition"
-                    >
-                        Clear
-                    </button>
-                </div>
-            </form>
+        {/* Storage */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Storage
+          </label>
+          <input
+            type="text"
+            name="storage"
+            placeholder="Enter new storage capacity"
+            value={newStorage}
+            className="input-style"
+            onChange={(e) => setNewStorage(e.target.value)}
+          />
         </div>
-    );
+
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Price
+          </label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Enter product price"
+            value={price}
+            className="input-style"
+            onChange={(e) => setPrice(e.target.value)}
+            min="0"
+            required
+          />
+        </div>
+        {/* Processor */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Processor
+          </label>
+          <input
+            type="text"
+            name="processor"
+            placeholder="Enter processor details"
+            value={processor}
+            className="input-style"
+            onChange={(e) => setProcessor(e.target.value)}
+          />
+        </div>
+
+        {/* RAM */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">RAM</label>
+          <input
+            type="text"
+            name="ram"
+            placeholder="Enter RAM size"
+            value={ram}
+            className="input-style"
+            onChange={(e) => setRam(e.target.value)}
+          />
+        </div>
+
+        {/* Battery */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Battery
+          </label>
+          <input
+            type="text"
+            name="battery"
+            placeholder="Enter battery capacity"
+            value={battery}
+            className="input-style"
+            onChange={(e) => setBattery(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Screen Size
+          </label>
+          <input
+            type="text"
+            name="battery"
+            placeholder="Enter Screen Size"
+            value={screen_size}
+            className="input-style"
+            onChange={(e) => setScreen_size(e.target.value)}
+          />
+        </div>
+
+        {/* Camera */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Camera
+          </label>
+          <input
+            type="text"
+            name="camera"
+            placeholder="Enter camera specifications"
+            value={camera}
+            className="input-style"
+            onChange={(e) => setCamera(e.target.value)}
+          />
+        </div>
+
+        {/* Stock */}
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Stock
+          </label>
+          <input
+            type="number"
+            name="stock"
+            placeholder="Enter stock quantity"
+            value={stock}
+            className="input-style"
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
+
+        <div className="w-full gap-4 mt-4 grid grid-cols-2 justify-center items-center">
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-lg transition"
+          />
+          <button
+            type="button"
+            onClick={handleClear}
+            className="bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-2 rounded-lg transition"
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UpdateSpec;
